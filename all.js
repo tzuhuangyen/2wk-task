082aabe9e067c773b7bf8ecded7a4648274730e8
tzuhuangyen <912yan@gmail.com>
2023-10-28 22:11:21 +0200

    rwd

---
+++ all.js
@@ -0,0 +1,8 @@
+/* 监听页面加载事件 */
+window.addEventListener("load", function () {
+  // 隐藏 Spinner
+  document.getElementById("spinnerContainer").style.display = "none";
+});
+
+// 显示 Spinner
+document.getElementById("spinnerContainer").style.display = "block";
